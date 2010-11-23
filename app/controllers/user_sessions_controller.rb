1aea9e23031dc007ab9188391a68154a8bf2580a
Jon Dean <jon@jonathandean.com>
2010-11-23 09:49:53 -0500

    Adding missing methods in the example

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -21,4 +21,22 @@ class UserSessionsController < ApplicationController
     flash[:notice] = "Logout successful!"
     redirect_back_or_default new_user_session_url
   end
+  
+  private
+    def require_user 
+      unless current_user 
+        store_location 
+        flash[:notice] = "You must be logged in to access this page" 
+        redirect_to login_path 
+        return false 
+      end 
+    end 
+    def require_no_user 
+      if current_user 
+        store_location 
+        flash[:notice] = "You cannot access this page while logged in to another account" 
+        redirect_to root_url 
+        return false 
+      end 
+    end
 end
